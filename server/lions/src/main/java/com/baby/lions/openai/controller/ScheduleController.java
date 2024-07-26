ca9f1298f0d646c6c661b46e5aab3648c47ed2e6
Sungwo Jang <131230124+Sungw0o@users.noreply.github.com>
2024-07-26 21:59:51 +0900

    [Feat] : 추천 일정 반환 로직 구현

--- server/lions/src/main/java/com/baby/lions/openai/controller/ScheduleController.java
+++ server/lions/src/main/java/com/baby/lions/openai/controller/ScheduleController.java
@@ -6,7 +6,6 @@ import com.baby.lions.openai.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
