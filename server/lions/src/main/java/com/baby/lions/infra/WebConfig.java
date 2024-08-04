1f7a503dbfd62a478f5d3163aedcbdf6185c4abb
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-04 16:37:22 +0900

    refactor

--- server/lions/src/main/java/com/baby/lions/infra/WebConfig.java
+++ server/lions/src/main/java/com/baby/lions/infra/WebConfig.java
@@ -1,21 +1,26 @@
 package com.baby.lions.infra;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-		@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**")
-							.allowedOrigins(
-											"http://localhost:8080",
-											"http://localhost:3000",
-											"http://localhost:5173"
-							)
-							.allowedMethods("GET", "POST")
-							.allowCredentials(true)
-							.maxAge(3000);
-		}
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOriginPattern("*");
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
