50ef566df7f6f80221ce68ad63c001d6119901dd
bwng0v0 <ehs4742@gmail.com>
2024-08-05 21:31:30 +0900

    [feat] 일정 수정기능

---
+++ front/lions/src/component/Modify.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { X } from 'lucide-react'
+import moment from "moment";
+import axios from "axios";
+
+export default function Modify(props) {
+
+    const [name, setname] = useState(props.cur.title);
+    const [time1, setTime1] = useState(props.cur.startTime);
+    const [time2, setTime2] = useState(props.cur.endTime);
+    const handle1 = (event)=>{ setTime1(event.target.value); console.log(event.target.value); }
+    const handle2 = (event)=>{ setTime2(event.target.value); console.log(event.target.value); }
+
+    function onChange(event) {
+        // 매개변수 'event'는 이벤트가 발생한 태그의 정보를 가져온다.
+        setname(event.target.value)
+        console.log(event.target.value);
+        // 값이 바뀔때마다 setname으로 name값을 변경해준다.
+    }
+    function update() {
+        let data = props.cur;
+        data.title = name;
+        data.startTime = time1;
+        data.endTime = time2;
+        axios.patch('https://babylion-api.yeongmin.kr/calendar/update',data)
+        .then(()=>{})
+        .catch(()=>{});
+
+        console.log(data);
+        props.setisOpen(false);
+    }
+
+    return (
+        <Container onClick={() => props.setisOpen(false)}>
+            <Contents onClick={(event) => event.stopPropagation()}>
+
+                <Top><h2>일정 수정</h2> <X onClick={() => props.setisOpen(false)} strokeWidth='1.6' /></Top>
+                <Input type='text' onChange={onChange} placeholder="제목" defaultValue={props.cur.title}/>
+                <input type="time" value={time1}  onChange={handle1} />
+                <input type="time" value={time2}    onChange={handle2} />
+
+                <div onClick={update}><p>추가하기</p></div>
+                {/* 위 온클릭 안에는 update가 들어가야함 */}
+            </Contents>
+        </Container>
+    )
+}
+
+//선택한 날짜에 일정 추가
+//일정 이름, 시작시간, 종료시간 입력받아서 부모 컴포넌트 객체에 넣기
+
+const Container = styled.div`
+    background-color: rgba(0, 0, 0, 0.4);
+    width: 100%;
+    height: 100svh;
+    z-index: 10;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+`
+const Contents = styled.div`
+    width: 300px;
+    height: 300px;
+    background-color: white;
+    margin: 0 auto;
+    border-radius: 16px;
+    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+    padding: 20px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+const Top = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+const Input = styled.input`
+    width: 80%;
+    height: 30px;
+    font-size: 20px;
+`
+
+const Submit = styled.button`
+    width: 100%;
+    font-size: 17px;
+    border: none;
+    background-color: #10B981;
+    color: white;
+    border-radius: 19px;
+    p{
+        margin: 10px;
+    }
+`
